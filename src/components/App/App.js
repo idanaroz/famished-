9db78403cd8f59411db1d5acf328b7ac4178967b
idanaroz <idanaroz@gmail.com>
2019-08-27 16:17:03 +0300

    Create React famished project. -Create the components

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+import BusinessList from '../BusinessList/BusinessList.js';
+import SearchBar from '../SearchBar/SearchBar.js';
+
+
+
+class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList /> 
+      </div>
+    );
+  }
+}
+
+export default App;
